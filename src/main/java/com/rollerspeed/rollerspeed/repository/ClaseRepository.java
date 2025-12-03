e98eda7b06bd7884ce37fd40b5994f582e5404cf
Jacquelie-jv <jacqueline.jimenez@est.iudigital.edu.co>
2025-12-02 21:12:21 -0500

    Actualizo User, SecurityConfig, AuthController, agrego Clase y nuevas vistas

---
+++ src/main/java/com/rollerspeed/rollerspeed/repository/ClaseRepository.java
@@ -0,0 +1,8 @@
+package com.rollerspeed.rollerspeed.repository;
+
+import com.rollerspeed.rollerspeed.Model.Clase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClaseRepository extends JpaRepository<Clase, Long> {
+
+}
